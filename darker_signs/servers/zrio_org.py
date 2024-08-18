4d4a7b7df07531204bb64638d16179a8a03d47f7
gira93 <23706223+gira93@users.noreply.github.com>
2024-08-18 16:34:54 +0200

    Added zrio server

---
+++ darker_signs/servers/zrio_org.py
@@ -0,0 +1,126 @@
+from getpass import getuser
+from termcolor import cprint
+from darker_signs.dns import Dns
+from darker_signs.mail import Mail
+from darker_signs.utils import download_file, upload_file
+
+
+class ZrioOrg:
+    def __init__(self, root_path: str, mail: Mail, dns: Dns) -> None:
+        self.root_path = root_path
+        self.mail = mail
+        self.dns = dns
+
+    def zrio_filetransfer(self):
+        cprint("Connected to ZRIO File Transfer", "green")
+        confirm = input("Would you like to upload a file to our server? (y/n): ")
+        if confirm != "y":
+            cprint("Connection closed by server", "red")
+            return
+        while True:
+            file = input(
+                "Enter complete file name or nothing to cancel (Eg: myfile.file): "
+            )
+            if file == "":
+                break
+            if upload_file(f"{self.root_path}/{file}"):
+                match file:
+                    case "8m.enc":
+                        message = "Thanks for the file, it's good to have you on our side.\nUnfortunately, there's still a bit of a problem.\nYou see, the file you got for us is highly encrypted.\nAre you able to decrypt it?\n\nPerhaps you could try using the tools at decrypt.com...\nWhen asked who to send it to, type in ZRIO.\n\nHope to hear from you again."
+                        self.mail.add_message(
+                            from_user="zrio.org", subject="8m.enc", message=message
+                        )
+                        break
+                    case "cryptsetup.exe":
+                        message = "Excellent work in getting the decryption software for us.\nWe have decrypted the hard drive.\nThere is one file we thought you should know about and have hosted it at ZRIO.org port 45.\nUsername zrio\nPassword geno38"
+                        self.mail.add_message(
+                            from_user="zrio.org",
+                            subject="cryptsetup.exe",
+                            message=message,
+                        )
+                        break
+                    case _:
+                        cprint(
+                            "That's not the file we expect, please refrain from sending unwanted files.",
+                            "red",
+                        )
+                        continue
+            else:
+                cprint("File not found", "red")
+                continue
+
+    def xcapro(self):
+        user = input("Username: ")
+        pw = input("Password: ")
+        if user == "zrio" and pw == "geno38":
+            self.__geno38_message()
+        elif user == "zrio" and pw == "geno91":
+            self.__geno91_message()
+        else:
+            return
+
+    def __geno38_message(self):
+        print()
+        print("To: Alan Johnson(alanjohnson@fbi.gov)")
+        print("Document: 22643")
+        print("Encryption: 7")
+        print()
+        print(
+            "We have a problem, there is a renegade group calling themselves ZRIO. They have hired"
+        )
+        print(
+            "a number of hackers to hack into goverment servers and gain as much information as they"
+        )
+        print(
+            f"can. One hacker in particular going by the alias of {getuser()} has been the most active."
+        )
+        print(
+            "We need this problem handled in the usual way. We cant let anyone know about project"
+        )
+        print(
+            "Genocide at any cost. Doing so would bring caos to the world over. We need this"
+        )
+        print(
+            "sorted out right away, we have sent the usual backup email just in case this package"
+        )
+        print("does not arrive as planed.")
+        print()
+        print("----------")
+        print()
+        print(
+            "For your own sakes you need to make sure this email doesn't reach the intended inbox."
+        )
+        print("Take alook at crackertools.com they usually have helpful tools.")
+        print()
+        print("- ZRIO")
+
+    def __geno91_message(self):
+        ips = [
+            "103.45.32.3",
+            "72.32.23.54",
+            "194.20.37.51",
+            "130.239.47.117",
+            "215.170.237.35",
+            "193.228.123.186",
+            "23.22.213.146",
+            "98.90.114.41",
+            "159.114.182.232",
+            "188.174.32.138",
+            "115.113.51.237",
+            "71.20.174.218",
+            "73.121.96.239",
+            "92.183.28.121",
+            "71.100.81.138",
+            "92.164.16.23",
+            "145.217.69.76",
+            "28.137.198.15",
+            "49.78.59.115",
+            "120.106.216.70",
+            "51.136.67.87",
+            "103.45.33.3",
+            "-----------------------------",
+            "Once you have the installer, upload it to ZRIO.org",
+        ]
+        download_file(f"{self.root_path}/ips.txt", "\n".join(ips))
+        cprint("File downloaded to ips.txt", "green")
+        return
