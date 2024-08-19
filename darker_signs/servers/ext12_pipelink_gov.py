ab7b5103ee37b9b6fe069e288e644e2d68a817d7
gira93 <23706223+gira93@users.noreply.github.com>
2024-08-19 21:42:33 +0200

    Pipelink + scan fixes

---
+++ darker_signs/servers/ext12_pipelink_gov.py
@@ -0,0 +1,25 @@
+from termcolor import cprint
+from darker_signs.dns import Dns
+from darker_signs.mail import Mail
+
+
+class Ext12PipelinkGov:
+    def __init__(self, root_path: str, mail: Mail, dns: Dns) -> None:
+        self.root_path = root_path
+        self.mail = mail
+        self.dns = dns
+
+    def pipelink_networking(self):
+        cprint("Error: Corrupt network data", "red")
+        cprint("WSOMISLMSSHLISHSIHAMISSHSASA", "yellow")
+        cprint("SDJSHLMDIAHIAHSGHASSSDUOSSDU", "light_yellow")
+        cprint("JSDHKSMHKWMHWHGJWGMJGJWHGNK", "red")
+        cprint("JHWQGJKQFT________FFZBJSTNFHYJSFNJSTNSG", "blue")
+        cprint("AKYGKGNKSYGNDSYGNHJSGNHJGHGS", "light_yellow")
+        cprint("JKNSSHAREDON66.1.24.JAISJHSNJHSJ", "blue")
+        cprint("HJGSKJMGDGSKYGSJDGSJHGJHGJHSS", "green")
+        cprint("JSHDKJSGDJSHGDJKSGJDGSJ__________________________HGKJSS", "blue")
+        cprint("SJKD____HKSHDJHGNGVZVJVDGHSVHGSS", "red")
+        cprint("SHDKSDHBJKHAGHNSHGAVNSGHAVJH", "light_yellow")
+        cprint("SLHKSDGJVFJHGVNDGVDNHJSGJHGH", "yellow")
+        cprint("QUGSIYUGNUYMGXKZNGZUYGNUYGZ", "white")
--- darker_signs/servers/scan_com.py
+++ darker_signs/servers/scan_com.py
@@ -1,7 +1,8 @@
 import os
 from ipaddress import AddressValueError, IPv4Address
 from termcolor import cprint
-from time import sleep
+
+from darker_signs.utils import progress_bar
 from ..mail import Mail
 from ..dns import Dns
 
@@ -13,9 +14,9 @@ class ScanCom:
         self.dns = dns
 
     def q(self):
-        cprint("/////////////////", "light_red")
-        cprint("///// QUAERO ////", "light_red")
-        cprint("/////////////////", "light_red")
+        cprint("//////////////////", "light_red")
+        cprint("///// QUAERO /////", "light_red")
+        cprint("//////////////////", "light_red")
         print("Ip Scanning tool")
         while True:
             start = input("Enter start IP address: ")
@@ -27,21 +28,21 @@ class ScanCom:
                     raise AddressValueError
             except AddressValueError:
                 print("Invalid addresses specified")
-                continue
+                return
             file = input("Save result to file (type a name): ")
             if file == "":
                 cprint("No file specified", "red")
-                continue
+                return
             break
         os.system("cls||clear")
         current = start
         print()
         with open(f"{self.root_path}/{file}", "w") as f:
             while current <= end:
-                sleep(0.01)
-                print(f"\r{current}", end="")
                 if self.dns.find(str(current)):
                     f.write(f"{current}\n")
                 current += 1
+            progress_bar(step=0.4)
+        print()
         cprint(f"Scan completed to file {file}", "green")
         print("Disconnected by server")
