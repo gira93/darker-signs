b8b2deb1488e464a563117ee8ccbf7f38c7ae238
gira93 <23706223+gira93@users.noreply.github.com>
2024-10-11 15:30:51 +0200

    Auth and mailer server

--- mother/servers/base_server.py
+++ mother/servers/base_server.py
@@ -1,27 +1,31 @@
 from typing import TypedDict
 from termcolor import cprint
+from pyfiglet import Figlet
 from system.mail import Mail
 from system.dns import Dns
 from system.player import Player
 from system.utils import progress_bar, show_menu
 
-Email = TypedDict("Email", {"from": str, "to": str, "content": str})
+Email = TypedDict("Email", {"from": str, "subject": str, "content": str})
 File = TypedDict("File", {"name": str, "content": str})
-Article = TypedDict("Article", {"title": str, "content": str})
 ChatMessage = TypedDict("ChatMessage", {"op": str, "content": str})
+DbEntry = TypedDict("DbEntry", {"title": str, "content": str})
 
 
 class ServerConfig(TypedDict):
     id: str
+    name: str
+    font: str | None
     banner: str
     writable: bool
+    authentication: list[tuple[str, str]] | None
     crashed: bool
     hack_tool: str | None
     defense_tool: str | None
 
 
 class WebServerConfig(ServerConfig):
-    contents: list[Article]
+    contents: list[DbEntry]
 
 
 class FileServerConfig(ServerConfig):
@@ -29,11 +33,15 @@ class FileServerConfig(ServerConfig):
 
 
 class MailServerConfig(ServerConfig):
-    contents: list[Email]
+    contents: dict[str, list[Email]]
 
 
 class ChatServerConfig(ServerConfig):
     contents: list[ChatMessage]
+
+
+class DbServerConfig(ServerConfig):
+    contents: list[DbEntry]
 
 
 class BaseServer:
@@ -56,15 +64,15 @@ class BaseServer:
             return
 
         self.__initialize_tools(server_config)
-        self.__welcome(config["banner"])
-        articles: list[Article] = config["contents"]
+        self.__welcome(config["banner"], config["name"], config["font"])
+        articles: list[DbEntry] = config["contents"]
         options = list(map(lambda article: article["title"], articles))
         while True:
             print("Available articles:")
             user_input = show_menu(options)
             match user_input:
                 case s if s.isdigit() and 1 <= int(s) <= len(options):
-                    article: Article = articles[int(s) - 1]
+                    article: DbEntry = articles[int(s) - 1]
                     print()
                     cprint(article["title"], "blue")
                     print()
@@ -72,10 +80,11 @@ class BaseServer:
                     print()
                     input("Press a key")
                     continue
-                case "nukelord" if self.player.has_tool("nukelord"):
+                case "nukelord" if self.player.has_tool("nukelord") and config[
+                    "hack_tool"
+                ] == "nukelord":
                     print()
                     cprint("NUKING", "red")
-                    print()
                     progress_bar()
                     cprint("SERVER NUKED", "green")
                     cprint("Disconnecting", "red")
@@ -90,8 +99,54 @@ class BaseServer:
                     continue
 
     def mail_server(self, server_config: ServerConfig):
-        banner = server_config["banner"]
-        self.__welcome(banner)
+        id, config = self.__load_config(server_config)
+        self.__initialize_tools(server_config)
+        self.__welcome(config["banner"], config["name"], config["font"])
+        account = self.__authenticate(config)
+
+        if not account:
+            cprint("Disconnecting", "red")
+            return
+
+        messages: list[Email] = config["contents"][account]
+        while True:
+            options = list(map(lambda message: message["subject"], messages))
+            print("Inbox:")
+            user_input = show_menu(options)
+            match user_input:
+                case s if s.isdigit() and 1 <= int(s) <= len(options):
+                    message_index = int(s) - 1
+                    message: Email = messages[message_index]
+                    print()
+                    cprint(f'From: {message["from"]}', "blue")
+                    cprint(f'Subject: {message["subject"]}', "blue")
+                    print()
+                    print(message["content"])
+                    print()
+                    if config["writable"]:
+                        message_option = show_menu(
+                            ["Delete message"], "Go back", "Select an action"
+                        )
+                        match message_option:
+                            case "1":
+                                messages.pop(message_index)
+                                config["contents"][account] = messages
+                                self.player.add_or_update_server(id, config)
+                                print()
+                                cprint("Message deleted", "red")
+                                input("Press a key")
+                                continue
+                            case _:
+                                continue
+                    else:
+                        input("Press a key")
+                        continue
+                case "0":
+                    print()
+                    cprint("Connection closed", "red")
+                    break
+                case _:
+                    continue
 
     def chat_server(self, server_config: ServerConfig):
         banner = server_config["banner"]
@@ -121,6 +176,44 @@ class BaseServer:
         config = server_override if server_override else dict(server_config)
         return (id, config)
 
-    def __welcome(self, banner: str):
+    def __welcome(self, banner: str, name: str, font: str = "standard"):
+        f = Figlet(font)
+        cprint(f.renderText(name), "blue")
+        print()
         cprint(banner, "green")
         print()
+
+    def __authenticate(self, server_config: dict) -> str | None:
+        accounts: dict[str, str] = dict(server_config["authentication"])
+        cprint("Login required", "yellow")
+        print()
+        user = input("Username: ")
+        pw = input("Password: ")
+        if user in accounts and pw == accounts[user]:
+            print()
+            cprint("Access granted", "green")
+            print()
+            return user
+        elif (
+            pw == "rootbreaker"
+            and self.player.has_tool("rootbreaker")
+            and server_config["hack_tool"] == "rootbreaker"
+        ):
+            print()
+            cprint("CRACKING", "red")
+            progress_bar()
+            if user in accounts:
+                print()
+                cprint("Access granted", "green")
+                print()
+                cprint(f"Password is: {accounts[user]}", "yellow")
+                return user
+            else:
+                print()
+                cprint("User not found\nCracking failed", "red")
+                print()
+                return None
+        else:
+            print()
+            cprint("Invalid username or password", "red")
+            return None
--- mother/servers/test_com.py
+++ mother/servers/test_com.py
@@ -1,8 +1,11 @@
-from .base_server import BaseServer, WebServerConfig
+from .base_server import BaseServer, MailServerConfig, WebServerConfig
 
 SERVER_CONFIG: WebServerConfig = {
     "id": "test_com",
+    "name": "Testing Grounds",
     "banner": "Welcome to the Testing Server!",
+    "font": "slant",
+    "authentication": None,
     "contents": [
         {
             "title": "Test article",
@@ -19,7 +22,43 @@ SERVER_CONFIG: WebServerConfig = {
     "defense_tool": None,
 }
 
+MAIL_SERVER_CONFIG: MailServerConfig = {
+    "id": "test_com_mailer",
+    "name": "Testing Mailer",
+    "banner": "Welcome to the Testing Server Mailer!",
+    "font": "doom",
+    "authentication": [("franco", "franchi"), ("gino", "gini")],
+    "contents": {
+        "franco": [
+            {
+                "from": "gino@pizzi.com",
+                "subject": "Hai visto che roba!",
+                "content": "Hai visto che roba zio?",
+            },
+            {
+                "from": "paolo@frino.com",
+                "subject": "Non sapevo nulla",
+                "content": "Giuro che Mother funziona bene\nIo non ne sapevo nulla!",
+            },
+        ],
+        "gino": [
+            {
+                "from": "fabio@gastani.it",
+                "subject": "Ma Paolo Stagna?",
+                "content": "Che è successo a Stagna??",
+            }
+        ],
+    },
+    "writable": True,
+    "crashed": False,
+    "hack_tool": "rootbreaker",
+    "defense_tool": None,
+}
+
 
 class TestCom(BaseServer):
     def http(self):
         self.web_server(SERVER_CONFIG)
+
+    def mailer(self):
+        self.mail_server(MAIL_SERVER_CONFIG)
